451f22463d72d7fec5b2265a831e6bcdc7b75ef0
Ella <elahees97@gmail.com>
2023-09-11 12:11:12 -0400

    Querry1

---
+++ A1.sql
@@ -0,0 +1,15 @@
+WITH ActorFilmCounts AS (
+    SELECT
+        a.actor_id,
+        CONCAT(a.first_name, ' ', a.last_name) AS actor_name,
+        COUNT(fa.film_id) AS film_count
+    FROM actor a
+    JOIN film_actor fa ON a.actor_id = fa.actor_id
+    GROUP BY a.actor_id, actor_name
+)
+SELECT
+    actor_id,
+    actor_name,
+    film_count
+FROM ActorFilmCounts
+ORDER BY film_count DESC;
